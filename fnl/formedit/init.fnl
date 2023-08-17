934c63c7e06178219ffc90c0cc460b5e6a856592
Kyle Grierson <griersonky@gmail.com>
2023-08-17 10:07:20 +0100

    update api

--- fnl/formedit/init.fnl
+++ fnl/formedit/init.fnl
@@ -3,5 +3,7 @@
 (local select (require :formedit.select))
 (local wrap (require :formedit.wrap))
 (local raise (require :formedit.raise))
+(local barf (require :formedit.barf))
+(local slurp (require :formedit.slurp))
 
-{: find : insertion : select : wrap : raise}
+{: find : insertion : select : wrap : raise : barf : slurp}
--- lua/formedit/init.lua
+++ lua/formedit/init.lua
@@ -4,4 +4,6 @@ local insertion = require("formedit.insertion")
 local select = require("formedit.select")
 local wrap = require("formedit.wrap")
 local raise = require("formedit.raise")
-return {find = find, insertion = insertion, select = select, wrap = wrap, raise = raise}
+local barf = require("formedit.barf")
+local slurp = require("formedit.slurp")
+return {find = find, insertion = insertion, select = select, wrap = wrap, raise = raise, barf = barf, slurp = slurp}
